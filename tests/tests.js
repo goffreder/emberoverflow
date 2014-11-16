1911615380728bec596d701126bbb958a2f3090b
goffreder <goffreder@gmail.com>
2014-11-16 10:58:27 +0100

    5.1 Testing Index Route

--- tests/tests.js
+++ tests/tests.js
@@ -7,25 +7,31 @@ App.injectTestHelpers();
 
 // common QUnit module declaration
 module("Integration tests", {
-  setup: function() {
-    // before each test, ensure the application is ready to run.
-    Ember.run(App, App.advanceReadiness);
-  },
+    setup: function() {
+        // before each test, ensure the application is ready to run.
+        Ember.run(App, App.advanceReadiness);
+    },
 
-  teardown: function() {
-    // reset the application state between each test
-    App.reset();
-  }
+    teardown: function() {
+        // reset the application state between each test
+        App.reset();
+    }
 });
 
-// QUnit test case
-test("/", function() {
-  // async helper telling the application to go to the '/' route
-  visit("/");
+test("index page has a title and a list of questions", function() {
+    visit("/");
 
-  // helper waiting the application is idle before running the callback
-  andThen(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
-    equal(find("li").length, 3, "There are three items in the list");
-  });
+    andThen(function() {
+        equal(
+            find("h2").text(),
+            "Welcome to Emberoverflow",
+            "Application header is rendered"
+        );
+
+        equal(
+            find("ul:not(.nav) > li").length,
+            2,
+            "There are two questions in the list"
+        );
+    });
 });
