df0524cff9ea3b60ff628ddaa191e40694fee520
goffreder <goffreder@gmail.com>
2014-11-18 00:34:41 +0100

    6.1 Defining Fixtures

--- js/app.js
+++ js/app.js
@@ -1,30 +1,9 @@
 App = Ember.Application.create();
 
+App.ApplicationStore = DS.Store.extend({
+    adapter : DS.FixtureAdapter
+});
+
 App.Router.map(function() {
     this.route('about');
-});
-
-App.IndexRoute = Ember.Route.extend({
-    model: function() {
-        var questions = [
-            {
-                title : "How do I feed hamsters?",
-                author : "Tom Dale"
-            },
-            {
-                title : "Are humans insane?",
-                author : "Tomster the Hamster"
-            }
-        ];
-
-        return questions;
-    }
-});
-
-App.IndexController = Ember.ArrayController.extend({
-    siteTitle : "Welcome to Emberoverflow",
-
-    currentTime : function() {
-        return(new Date());
-    }.property()
 });
