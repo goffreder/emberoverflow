0c0c3c57f9df055ab6162a34107bc1301e0ab382
goffreder <goffreder@gmail.com>
2014-11-28 01:25:14 +0100

    8.5 Testing Signing-in

--- tests/tests.js
+++ tests/tests.js
@@ -9,6 +9,7 @@ App.injectTestHelpers();
 module("Integration tests", {
     setup: function() {
         // before each test, ensure the application is ready to run.
+        delete localStorage.currentUser;
         Ember.run(App, App.advanceReadiness);
         // hides the ember testing container if needed
         if(window.location.search.indexOf("&nocontainer=true") !== -1)
@@ -18,6 +19,7 @@ module("Integration tests", {
     teardown: function() {
         // reset the application state between each test
         App.reset();
+        delete localStorage.currentUser;
     }
 });
 
@@ -54,6 +56,21 @@ test("quesion links on index page lead to questions", function() {
             find("p").length,
             2,
             "Question and author are rendered"
+        );
+    });
+});
+
+test("user will be able to log in", function() {
+    visit("/sign-in");
+
+    fillIn(".form-control", "tomster@hamster.com");
+    click("button");
+
+    andThen(function() {
+        equal(
+            find("p").text(),
+            "You are already signed-in!",
+            "Signed-in message rendered"
         );
     });
 });
