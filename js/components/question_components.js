93a312afaf3b77f3cad0427b4da0aeaf989486c8
goffreder <goffreder@gmail.com>
2014-12-08 18:43:08 +0100

    11.1 Components

---
+++ js/components/question_components.js
@@ -0,0 +1,14 @@
+App.QuestionPreviewComponent = Ember.Component.extend({
+    tagName : 'li',
+
+    answersCount : Ember.computed.alias('question.answers.length'),
+
+    pluralForm : function() {
+        var answers = this.get('answersCount');
+        return answers === 1 ? 'answer' : 'answers';
+    }.property('answersCount'),
+
+    hasAnswers : function() {
+        return this.get('answersCount') > 0;
+    }.property('answersCount')
+});
