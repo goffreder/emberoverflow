4dedb002d200947db077bb1db4a29347ca12fb25
goffreder <goffreder@gmail.com>
2014-12-08 19:11:07 +0100

    11.4 Mighty Render (not working)

--- js/fixtures/question_fixtures.js
+++ js/fixtures/question_fixtures.js
@@ -5,7 +5,7 @@ App.Question.FIXTURES = [
         author : 201,
         date : '2013-01-01T12:00:00',
         question : 'Tomster cant eat using knife and a fork because his hands are too small. We are looking for a way to feed him. Any ideas?',
-        answers : [301]
+        answers : [301, 302]
     },
     {
         id : 102,
